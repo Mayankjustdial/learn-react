807f4d8593f78b5852be1fd5309a3b07971e723d
Mayank <mayankghz225@gmail.com>
2023-06-23 13:20:59 +0530

    Styling and CSS basics in react
    1. CSS stylesSheet
    2. Inline Styling
    3. CSS modules
    4. CSS in Js Libaries

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Counter from "./components/Counter";
 import EventBind from "./components/EventBind";
 import FunctionClick from "./components/FunctionClick";
 import GreetFunction from "./components/GreetFunction";
+import Inline from "./components/Inline";
 import Message from "./components/Message";
 import NameList from "./components/NameList";
 import ParentComponent from "./components/ParentComponent";
+import StyleSheet from "./components/StyleSheet";
 import UserGreeting from "./components/UserGreeting";
 import WelcomeClass from "./components/WelcomeClass";
+import styles from "./appStyles.module.css";
+import "./appStyles.css";
 
 function App() {
   return (
@@ -16,14 +20,19 @@ function App() {
         <h3>My name is mayank kumar</h3>
       </GreetFunction> */}
       {/* <WelcomeClass name="Kumar" heroName="Wonder Woman" /> */}
-      {/* <Message /> */}
+      {/* <Message />jj */}
       {/* <Counter /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
       {/* <EventBind /> */}
       {/* <ParentComponent /> */}
       {/* <UserGreeting /> */}
-      <NameList />
+      {/* <NameList /> */}
+      {/* <StyleSheet primary={false} /> */}
+      <Inline />
+
+      <h1 className="error">Error</h1>
+      <h1 className={styles.success}>Success</h1>
     </div>
   );
 }
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,7 +2,40 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
-  const names = ["Bruce", "Clark", "Diana", "Bruce"];
+  const names = ["Bruce", "Clark", "Diana"];
+
+  // const persons = [
+  //   {
+  //     id: 1,
+  //     name: "Mayank",
+  //     age: 22,
+  //     skill: "C++",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Kumar",
+  //     age: 21,
+  //     skill: "Java",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Sittu",
+  //     age: 22,
+  //     skill: "React",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "John",
+  //     age: 22,
+  //     skill: "JavaScript",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Doe",
+  //     age: 19,
+  //     skill: "php",
+  //   },
+  // ];
 
   const nameList = names.map((name, index) => (
     <h2 key={index}>
@@ -10,6 +43,12 @@ function NameList() {
     </h2>
   ));
 
+  // const personList = persons.map((person) => (
+  // <h3>
+  //   I am {person.name}. I am {person.age} years old and I know {person.skill}
+  // </h3>
+  // <Person key={person.id} person={person} />
+  //));
   return (
     <div>
       {/* {personList} */}

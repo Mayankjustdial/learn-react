9a213a50978e8a0e3a5fafa6cd3fde57f86d2cfe
Mayank <mayankghz225@gmail.com>
2023-06-24 12:53:33 +0530

    Post request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <PostList />
+        {/* <PostList /> */}
+        <PostForm />
       </div>
     );
   }

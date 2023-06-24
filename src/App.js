f73c2f81a101cb4a92b43a17be70f818a846d590
Mayank <mayankghz225@gmail.com>
2023-06-24 12:34:39 +0530

    Http get request using axios

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React, { Component } from "react";
+import PostList from "./components/PostList";
 
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <PostList />
+      </div>
+    );
+  }
+}
+
+export default App;

47730efb74c1adb03532e574c982344919037ca7
Mayank <mayankghz225@gmail.com>
2023-06-25 13:03:50 +0530

    Run Effects only once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ClassCounterOne from "./components/ClassCounterOne";
 import HookCounterOne from "./components/HookCounterOne";
+import ClassMouse from "./components/ClassMouse";
+import HookMouse from "./components/HookMouse";
 
 function App() {
   return (
     <div>
       {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }

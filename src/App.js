fb637d46766bc80344d5cb8ed25379c8fe84e1a3
Mayank <mayankghz225@gmail.com>
2023-06-25 13:21:55 +0530

    useEffect with cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import ClassCounterOne from "./components/ClassCounterOne";
-import HookCounterOne from "./components/HookCounterOne";
+
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
+import MouseContainer from "./components/MouseContainer";
 
 function App() {
   return (
     <div>
-      {/* <ClassCounterOne /> */}
-      {/* <HookCounterOne /> */}
-      {/* <ClassMouse /> */}
-      <HookMouse />
+      \{/* <ClassMouse /> */}
+      {/* <HookMouse /> */}
+      <MouseContainer />
     </div>
   );
 }

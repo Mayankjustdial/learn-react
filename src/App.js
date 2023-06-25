cdfe553de0ec43939b445a739ad9cdaeeaa0ab75
Mayank <mayankghz225@gmail.com>
2023-06-25 14:06:38 +0530

    useEffect with incorrect dependency

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 import ClassMouse from "./components/ClassMouse";
 import HookMouse from "./components/HookMouse";
 import MouseContainer from "./components/MouseContainer";
+import IntervalClassCounter from "./components/IntervalClassCounter";
+import IntervalHookCounter from "./components/IntervalHookCounter";
 
 function App() {
   return (
     <div>
-      \{/* <ClassMouse /> */}
+      {/* <ClassMouse /> */}
       {/* <HookMouse /> */}
-      <MouseContainer />
+      {/* <MouseContainer /> */}
+      <IntervalClassCounter />
+      <IntervalHookCounter />
     </div>
   );
 }
